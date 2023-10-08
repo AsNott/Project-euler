44998003f6130f6f65567868175b8ebef032196d
Nurullah Coşkun <97886064+AsNott@users.noreply.github.com>
2023-10-08 17:24:29 +0300

    Add files via upload

---
+++ project-euler/solution - 1/solution - 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace solution___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.
+
+            List<int> sayilar = new List<int>();
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sayilar.Add(i);
+                }
+            }
+
+            string sonuc = string.Join(", ", sayilar);
+            Console.WriteLine(sonuc);
+        }
+    }
+}
